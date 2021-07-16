419566ccedaf913fa85a18d424ddf630aa914b00
Mohamedstt <mohamedschof@gmail.com>
2021-07-16 12:39:36 +0100

    Header component.

---
+++ src/Header/index.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
+
+import styles from "./styles.module.scss";
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="logo" />
+        <nav>
+          <a href="" className={styles.active}>
+            Home
+          </a>
+          <a href="">Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
