88e6c8e63832ec6e41207777bcfc20e3509a1e31
Mohamedstt <mohamedschof@gmail.com>
2021-07-20 17:05:36 +0100

    GitHub authentication.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
+import { GetStaticProps } from "next";
+import { stripe } from "../services/stripe";
+
+import { SubscribeButton } from "../components/SubscribeButton";
+
 import styles from "./home.module.scss";
-export default function Home() {
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,11 +28,30 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            for <span>for $9.99 month</span>
+            for <span>{product.amount} month</span>
           </p>
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="girl coding" />
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JDsTzAniN8DM00cGzb8OAKf");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24,
+  };
+};
